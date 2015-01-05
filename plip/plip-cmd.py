2365e332011e8237cc2cbb37c554c7b4381b96d5
Sebastian Salentin <sebastian.salentin@biotec.tu-dresden.de>
2015-01-05 16:36:54 +0100

    Parses all additional options for thresholds.

--- plip/plip-cmd.py
+++ plip/plip-cmd.py
@@ -27,6 +27,7 @@ from modules.report import TextOutput
 
 # Python standard library
 import sys
+import argparse
 from argparse import ArgumentParser
 import urllib2
 import time
@@ -233,6 +234,17 @@ if __name__ == '__main__':
     parser.add_argument("--maxthreads", dest="maxthreads", default=1,
                         help="Set maximum number of main threads (number of binding sites processed simultaneously)",
                         type=int)
+    # Optional threshold arguments, not shown in help
+    for threshold in [('bs_dist_max', 6.0), ('aromatic_planarity', 7.5), ('hydroph_dist_max', 4.0),
+                      ('hbond_dist_max', 4.0), ('hbond_don_angle_min', 90.0), ('pistack_dist_max', 7.5),
+                      ('pistack_ang_dev', 30.0), ('pistack_offset_max', 2.0), ('pication_dist_max', 6.0),
+                      ('saltbridge_dist_max', 5.0), ('halogen_dist_max', 4.0), ('halogen_acc_angle', 120.0),
+                      ('halogen_don_angle', 165.0), ('halogen_angle_dev', 30.0), ('water_bridge_mindist', 2.5),
+                      ('water_bridge_maxdist', 4.0), ('water_bridge_omega_min', 80.0),
+                      ('water_bridge_omega_max', 160.0), ('water_bridge_theta_min', 100.0)]:
+        name, defaultval = threshold
+        parser.add_argument('--%s' % name, default=defaultval, dest=name, type=float, help=argparse.SUPPRESS)
+
     arguments = parser.parse_args()
 
     main(arguments)  # Start main script
