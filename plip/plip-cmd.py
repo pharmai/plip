effe738eedaf889114f15391f4314e78627e811c
Sebastian Salentin <sebastian.salentin@biotec.tu-dresden.de>
2015-01-23 13:35:25 +0100

    Changes version number

--- plip/plip-cmd.py
+++ plip/plip-cmd.py
@@ -37,7 +37,7 @@ import multiprocessing
 # External libraries
 import lxml.etree as et
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 descript = "Protein-Ligand Interaction Profiler (PLIP) v%s " \
            "is a command-line based tool to analyze interactions in a protein-ligand complex." % __version__
 
