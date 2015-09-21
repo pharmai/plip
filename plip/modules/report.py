d4d7beaf1e318d272d8b55976faa1c881daa3159
Sebastian Salentin <sebastian.salentin@biotec.tu-dresden.de>
2015-09-21 10:48:24 +0200

    Fixes issue with order of composite members in XML file. Is now identical to ordering internally and in longname.

--- plip/modules/report.py
+++ plip/modules/report.py
@@ -226,7 +226,7 @@ class TextOutput:
 
         txt = []
         txt.append('%s (%s) - %s' % (self.bsid, self.longname, self.ligtype))
-        for i, member in enumerate(sorted(self.lig_members)[1:]):
+        for i, member in enumerate(self.lig_members)[1:]:  # Already sorted by (chain, position)
             txt.append('  + %s' % ":".join(str(element) for element in member))
         txt.append("-" * len(self.bsid))
         txt.append("Interacting chain(s): %s\n" % ','.join([chain for chain in self.interacting_chains]))
