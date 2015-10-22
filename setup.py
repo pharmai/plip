aee9a6b087c50e4b05ddd95e65f2ad971196916f
Sebastian Salentin <sebastian.salentin@biotec.tu-dresden.de>
2015-10-22 14:10:54 +0200

    Changelog and version 1.2.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ limitations under the License.
 from setuptools import setup
 
 setup(name='plip',
-      version='1.2.0',
+      version='1.2.1',
       description='PLIP - Fully automated protein-ligand interaction profiler',
       classifiers=[
           'Development Status :: 5 - Production/Stable',
