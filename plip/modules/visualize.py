b2c9abb3ca20b7710e1ff3c0aee669f654d056a6
Sebastian Salentin <sebastian.salentin@biotec.tu-dresden.de>
2016-04-14 15:49:31 +0200

    Full support for Chimera Plugins.

--- plip/modules/visualize.py
+++ plip/modules/visualize.py
@@ -523,12 +523,18 @@ class PyMOLVisualizer:
 
 class ChimeraVisualizer():
     """Provides visualization for Chimera."""
-    def __init__(self, plcomplex, chimera_module):
+    def __init__(self, plcomplex, chimera_module, tid):
         self.chimera = chimera_module
+        self.tid = tid
+        self.uid = plcomplex.uid
+        self.plipname = 'PLIP-%i' % self.tid
+        self.hetid, self.chain, self.pos = self.uid.split(':')
+        self.pos = int(self.pos)
         Molecule = self.chimera.Molecule
         self.colorbyname = self.chimera.colorTable.getColorByName
         self.rc = self.chimera.runCommand
         self.getPseudoBondGroup = self.chimera.misc.getPseudoBondGroup
+
         if not plcomplex is None:
             self.plcomplex = plcomplex
             self.protname = plcomplex.pdbid  # Name of protein with binding site
@@ -536,25 +542,29 @@ class ChimeraVisualizer():
             self.metal_ids = plcomplex.metal_ids
             self.water_ids = []
             self.bs_res_ids = []
-
             self.models = self.chimera.openModels
 
-            #@TODO Properly select model (new plipmodel)
             for md in self.models.list():
-                print md.name, len(md.atoms)
-                if md.name.startswith('PLIP'):
+                if md.name == self.plipname:
                     self.model = md
-            self.helper_mol = Molecule()  # Chimera Molecule
-            self.helper_mol.name = 'PLIPHelper'
-            self.models.add([self.helper_mol])
 
             self.atoms = self.atom_by_serialnumber()
 
-    def set_initial_representations(self):
-        self.rc("background solid white")
 
-    def make_initial_selections(self):
-        pass
+    def set_initial_representations(self):
+        """Set the initial representations"""
+        self.update_model_dict()
+        self.rc("background solid white")
+        self.rc("setattr g display 0")  # Hide all pseudobonds
+        self.rc("~display #%i & :/isHet & ~:%s" % (self.model_dict[self.plipname],self.hetid))
+
+    def update_model_dict(self):
+        """Updates the model dictionary"""
+        dct = {}
+        models = self.chimera.openModels
+        for md in models.list():
+            dct[md.name] = md.id
+        self.model_dict = dct
 
     def atom_by_serialnumber(self):
         """Provides a dictionary mapping serial numbers to their atom objects."""
@@ -565,7 +575,7 @@ class ChimeraVisualizer():
 
     def show_hydrophobic(self):
         """Visualizes hydrophobic contacts."""
-        grp = self.getPseudoBondGroup("Hydrophobic Interactions", associateWith=[self.model])
+        grp = self.getPseudoBondGroup("Hydrophobic Interactions-%i" % self.tid, associateWith=[self.model])
         grp.lineType = self.chimera.Dash
         grp.lineWidth = 3
         grp.color = self.colorbyname('gray')
@@ -575,23 +585,21 @@ class ChimeraVisualizer():
 
     def show_hbonds(self):
         """Visualizes hydrogen bonds."""
-        grp = self.getPseudoBondGroup("Hydrogen Bonds", associateWith=[self.model])
+        grp = self.getPseudoBondGroup("Hydrogen Bonds-%i" % self.tid, associateWith=[self.model])
         grp.lineWidth = 3
         for i in self.plcomplex.hbonds.ldon_id:
             b = grp.newPseudoBond(self.atoms[i[0]], self.atoms[i[1]])
             b.color = self.colorbyname('blue')
             self.bs_res_ids.append(i[0])
-            print "HBOND", i
         for i in self.plcomplex.hbonds.pdon_id:
             b = grp.newPseudoBond(self.atoms[i[0]], self.atoms[i[1]])
             b.color = self.colorbyname('blue')
             self.bs_res_ids.append(i[1])
-            print "HBOND", i
 
 
     def show_halogen(self):
         """Visualizes halogen bonds."""
-        grp = self.getPseudoBondGroup("HalogenBonds", associateWith=[self.model])
+        grp = self.getPseudoBondGroup("HalogenBonds-%i" % self.tid, associateWith=[self.model])
         grp.lineWidth = 3
         for i in self.plcomplex.halogen_bonds:
             b = grp.newPseudoBond(self.atoms[i[0]], self.atoms[i[1]])
@@ -601,14 +609,13 @@ class ChimeraVisualizer():
 
     def show_stacking(self):
         """Visualizes pi-stacking interactions."""
-        grp = self.getPseudoBondGroup("pi-Stacking", associateWith=[self.model])
+        grp = self.getPseudoBondGroup("pi-Stacking-%i" % self.tid, associateWith=[self.model])
         grp.lineWidth = 3
         grp.lineType = self.chimera.Dash
         for i, stack in enumerate(self.plcomplex.pistacking):
-            #@TODO Only one new model for centroids, chargecenters etc.
 
-            m = self.helper_mol
-            r = m.newResidue("Centroids", " ", 1, " ")
+            m = self.model
+            r = m.newResidue("pseudoatoms", " ", 1, " ")
             centroid_prot = m.newAtom("CENTROID", self.chimera.Element("CENTROID"))
             x, y, z = stack.proteinring_center
             centroid_prot.setCoord(self.chimera.Coord(x, y, z))
@@ -627,13 +634,13 @@ class ChimeraVisualizer():
 
     def show_cationpi(self):
         """Visualizes cation-pi interactions"""
-        grp = self.getPseudoBondGroup("Cation-Pi", associateWith=[self.model])
+        grp = self.getPseudoBondGroup("Cation-Pi-%i" % self.tid, associateWith=[self.model])
         grp.lineWidth = 3
         grp.lineType = self.chimera.Dash
         for i, cat in enumerate(self.plcomplex.pication):
 
-            m = self.helper_mol
-            r = m.newResidue("Centroids2", " ", 1, " ")
+            m = self.model
+            r = m.newResidue("pseudoatoms", " ", 1, " ")
             chargecenter = m.newAtom("CHARGE", self.chimera.Element("CHARGE"))
             x, y, z = cat.charge_center
             chargecenter.setCoord(self.chimera.Coord(x, y, z))
@@ -655,13 +662,13 @@ class ChimeraVisualizer():
     def show_sbridges(self):
         """Visualizes salt bridges."""
         # Salt Bridges
-        grp = self.getPseudoBondGroup("Salt Bridges", associateWith=[self.model])
+        grp = self.getPseudoBondGroup("Salt Bridges-%i" % self.tid, associateWith=[self.model])
         grp.lineWidth = 3
         grp.lineType = self.chimera.Dash
         for i, sbridge in enumerate(self.plcomplex.saltbridges):
 
-            m = self.helper_mol
-            r = m.newResidue("Centroids3", " ", 1, " ")
+            m = self.model
+            r = m.newResidue("pseudoatoms", " ", 1, " ")
             chargecenter1 = m.newAtom("CHARGE", self.chimera.Element("CHARGE"))
             x, y, z = sbridge.positive_center
             chargecenter1.setCoord(self.chimera.Coord(x, y, z))
@@ -682,7 +689,7 @@ class ChimeraVisualizer():
 
     def show_wbridges(self):
         """Visualizes water bridges"""
-        grp = self.getPseudoBondGroup("Water Bridges", associateWith=[self.model])
+        grp = self.getPseudoBondGroup("Water Bridges-%i" % self.tid, associateWith=[self.model])
         grp.lineWidth = 3
         for i, wbridge in enumerate(self.plcomplex.waterbridges):
             c = grp.newPseudoBond(self.atoms[wbridge.water_id], self.atoms[wbridge.acc_id])
@@ -698,8 +705,7 @@ class ChimeraVisualizer():
 
     def show_metal(self):
         """Visualizes metal coordination."""
-        # Metal Coordination
-        grp = self.getPseudoBondGroup("Metal Coordination", associateWith=[self.model])
+        grp = self.getPseudoBondGroup("Metal Coordination-%i" % self.tid, associateWith=[self.model])
         grp.lineWidth = 3
         for i, metal in enumerate(self.plcomplex.metal_complexes):
             c = grp.newPseudoBond(self.atoms[metal.metal_id], self.atoms[metal.target_id])
@@ -719,21 +725,29 @@ class ChimeraVisualizer():
             water_selection = []
             for wid in self.water_ids:
                 water_selection.append('serialNumber=%i' % wid)
-            self.rc("~display :HOH & ~:@/%s" % " or ".join(water_selection))
+            self.rc("~display :HOH")
+            self.rc("display :@/%s" % " or ".join(water_selection))
 
         # Show all interacting binding site residues
+        self.rc("~display #%i & ~:/isHet" % self.model_dict[self.plipname])
         self.rc("display :%s" % ",".join([str(self.atoms[bsid].residue.id) for bsid in self.bs_res_ids]))
+        self.rc("color lightblue :HOH")
+
 
 
     def zoom_to_ligand(self):
-        pass
+        """Centers the view on the ligand and its binding site residues."""
+        self.rc("center #%i & :%s" % (self.model_dict[self.plipname], self.hetid))
 
     def refinements(self):
         """Details for the visualization."""
         self.rc("setattr a color gray @CENTROID")
         self.rc("setattr a radius 0.3 @CENTROID")
+        self.rc("represent sphere @CENTROID")
         self.rc("setattr a color orange @CHARGE")
-        self.rc("setattr a radius 0.5 @CHARGE")
+        self.rc("setattr a radius 0.4 @CHARGE")
+        self.rc("represent sphere @CHARGE")
+        self.rc("display :pseudoatoms")
 
 
 
