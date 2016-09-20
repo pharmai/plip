6a9088a8772f893aac08558bf69388d41791d658
Sebastian Salentin <sebastian.salentin@biotec.tu-dresden.de>
2016-09-20 11:16:49 +0200

    Integrates module for parsing PLIP XML files. Adds unit tests and test file for XML parser.

---
+++ plip/modules/plipxml.py
@@ -0,0 +1,287 @@
+"""
+Protein-Ligand Interaction Profiler - Analyze and visualize protein-ligand interactions in PDB files.
+plipxml.py - Read in PLIP XML files for further analysis.
+Copyright 2014-2016 Sebastian Salentin
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+# Python standard library
+from collections import namedtuple, defaultdict
+from lxml import etree
+import itertools
+from itertools import groupby
+import sys
+
+class XMLStorage:
+    """Generic class for storing XML data from PLIP XML files."""
+
+    def getdata(self, tree, location):
+        """Gets XML data from a specific element and handles types."""
+        found = tree.xpath('%s/text()' % location)
+        if found == []:
+            return None
+        else:
+            data = found[0]
+        if data == 'True':
+            return True
+        elif data == 'False':
+            return False
+        else:
+            try:
+                return int(data)
+            except ValueError:
+                try:
+                    return float(data)
+                except ValueError:
+                    # It's a string
+                    return data
+
+    def getcoordinates(self, tree, location):
+        """Gets coordinates from a specific element in PLIP XML"""
+        return tuple(float(x) for x in tree.xpath('.//%s/*/text()' % location))
+
+
+class Interaction(XMLStorage):
+    """Stores information on a specific interaction type"""
+
+    def __init__(self, interaction_part):
+        self.id = interaction_part.get('id')
+        self.resnr = self.getdata(interaction_part, 'resnr')
+        self.restype = self.getdata(interaction_part, 'restype')
+        self.reschain = self.getdata(interaction_part, 'reschain')
+        self.ligcoo = self.getcoordinates(interaction_part, 'ligcoo')
+        self.protcoo = self.getcoordinates(interaction_part, 'protcoo')
+
+
+
+class HydrophobicInteraction(Interaction):
+    """Stores information on a hydrophobic interaction"""
+
+    def __init__(self, hydrophobic_part):
+        Interaction.__init__(self, hydrophobic_part)
+        self.dist = self.getdata(hydrophobic_part, 'dist')
+        self.ligcarbonidx = self.getdata(hydrophobic_part, 'ligcarbonidx')
+        self.protcarbonidx = self.getdata(hydrophobic_part, 'protcarbonidx')
+
+
+class HydrogenBond(Interaction):
+    """Stores information on a hydrogen bond interaction"""
+
+    def __init__(self, hbond_part):
+        Interaction.__init__(self, hbond_part)
+        self.sidechain = self.getdata(hbond_part, 'sidechain')
+        self.dist_h_a = self.getdata(hbond_part, 'dist_h-a')
+        self.dist_d_a = self.getdata(hbond_part, 'dist_d-a')
+        self.dist = self.dist_d_a
+
+        self.don_angle = self.getdata(hbond_part, 'don_angle')
+        self.protisdon = self.getdata(hbond_part, 'protisdon')
+        self.donoridx = self.getdata(hbond_part, 'donoridx')
+        self.acceptoridx = self.getdata(hbond_part, 'acceptoridx')
+        self.donortype = self.getdata(hbond_part, 'donortype')
+        self.acceptortype = self.getdata(hbond_part, 'acceptortype')
+
+
+class WaterBridge(Interaction):
+    """Stores information on a water bridge interaction"""
+
+    def __init__(self, wbridge_part):
+        Interaction.__init__(self, wbridge_part)
+        self.dist_a_w = self.getdata(wbridge_part, 'dist_a-w')
+        self.dist_d_w = self.getdata(wbridge_part, 'dist_d-w')
+        self.don_angle = self.getdata(wbridge_part, 'don_angle')
+        self.water_angle = self.getdata(wbridge_part, 'water_angle')
+        self.protisdon = self.getdata(wbridge_part, 'protisdon')
+        self.dist = self.dist_a_w if self.protisdon else self.dist_d_w
+
+        self.donor_idx = self.getdata(wbridge_part, 'donor_idx')
+        self.acceptor_idx = self.getdata(wbridge_part, 'acceptor_idx')
+        self.donortype = self.getdata(wbridge_part, 'donortype')
+        self.acceptortype = self.getdata(wbridge_part, 'acceptortype')
+        self.water_idx = self.getdata(wbridge_part, 'water_idx')
+        self.watercoo = self.getcoordinates(wbridge_part, 'watercoo')
+
+
+class SaltBridge(Interaction):
+    """Stores information on a salt bridge interaction"""
+
+    def __init__(self, sbridge_part):
+        Interaction.__init__(self, sbridge_part)
+        self.dist =  self.getdata(sbridge_part, 'dist')
+        self.protispos = self.getdata(sbridge_part, 'protispos')
+        self.lig_group = self.getdata(sbridge_part, 'lig_group')
+        self.lig_idx_list = [int(tagpart.text) for tagpart in
+                             sbridge_part.xpath('lig_idx_list/idx')]
+
+
+class PiStacking(Interaction):
+    """Stores information on a pi stacking interaction"""
+
+    def __init__(self, pistack_part):
+        Interaction.__init__(self, pistack_part)
+        self.centdist = self.getdata(pistack_part, 'centdist')
+        self.dist = self.centdist
+        self.angle = self.getdata(pistack_part, 'angle')
+        self.offset = self.getdata(pistack_part, 'offset')
+        self.type = self.getdata(pistack_part, 'type')
+        self.lig_idx_list = [int(tagpart.text) for tagpart in
+                             pistack_part.xpath('lig_idx_list/idx')]
+
+
+class PiCation(Interaction):
+    """Stores information on a pi cation interaction"""
+
+    def __init__(self, pication_part):
+        Interaction.__init__(self, pication_part)
+        self.dist = self.getdata(pication_part, 'dist')
+        self.offset = self.getdata(pication_part, 'offset')
+        self.protcharged = self.getdata(pication_part, 'protcharged')
+        self.lig_group = self.getdata(pication_part, 'lig_group')
+        self.lig_idx_list = [int(tag.text) for tag in pication_part.xpath('.//lig_idx_list/idx')]
+
+
+class HalogenBond(Interaction):
+    """Stores information on a halogen bond interaction"""
+
+    def __init__(self, halogen_part):
+        Interaction.__init__(self, halogen_part)
+        self.dist = self.getdata(halogen_part, 'dist')
+        self.don_angle = self.getdata(halogen_part, 'don_angle')
+        self.acc_angle = self.getdata(halogen_part, 'acc_angle')
+        self.donortype = self.getdata(halogen_part, 'donortype')
+        self.acceptortype = self.getdata(halogen_part, 'acceptortype')
+        self.don_idx = self.getdata(halogen_part, 'don_idx')
+        self.acc_idx = self.getdata(halogen_part, 'acc_idx')
+        self.sidechain = self.getdata(halogen_part, 'sidechain')
+
+
+class MetalComplex(Interaction):
+    """Stores information on a metal complexe interaction"""
+
+    def __init__(self, metalcomplex_part):
+        Interaction.__init__(self, metalcomplex_part)
+        self.metal_idx = self.getdata(metalcomplex_part, 'metal_idx')
+        self.metal_type = self.getdata(metalcomplex_part, 'metal_type')
+        self.target_idx = self.getdata(metalcomplex_part, 'target_idx')
+        self.target_type = self.getdata(metalcomplex_part, 'target_type')
+        self.coordination = self.getdata(metalcomplex_part, 'coordination')
+        self.dist = self.getdata(metalcomplex_part, 'dist')
+        self.location = self.getdata(metalcomplex_part, 'location')
+        self.rms = self.getdata(metalcomplex_part, 'rms')
+        self.geometry = self.getdata(metalcomplex_part, 'geometry')
+        self.complexnum = self.getdata(metalcomplex_part, 'complexnum')
+        self.targetcoo = self.getcoordinates(metalcomplex_part, 'targetcoo')
+        self.metalcoo = self.getcoordinates(metalcomplex_part, 'metalcoo')
+
+class BSite(XMLStorage):
+    """Stores all information about an specific binding site."""
+
+    def __init__(self, bindingsite, pdbid):
+        self.bindingsite = bindingsite
+        self.pdbid = pdbid
+        self.bsid = ":".join(bindingsite.xpath('identifiers/*/text()')[2:5])
+        self.uniqueid = ":".join([self.pdbid, self.bsid])
+        self.hetid = self.getdata(bindingsite, 'identifiers/hetid')
+        self.longname = self.getdata(bindingsite, 'identifiers/longname')
+        self.ligtype = self.getdata(bindingsite, 'identifiers/ligtype')
+        self.smiles = self.getdata(bindingsite, 'identifiers/smiles')
+
+        # Information on binding site members
+        self.members = []
+        for member in bindingsite.xpath('identifiers/members/member'):
+            self.members += member.xpath('text()')
+
+        self.composite = True if self.getdata(bindingsite, 'identifiers/composite') == 'True' else False
+
+        # Ligand Properties
+        self.heavy_atoms = self.getdata(bindingsite, 'lig_properties/num_heavy_atoms')
+        self.hbd = self.getdata(bindingsite, 'lig_properties/num_hbd')
+        self.unpaired_hbd = self.getdata(bindingsite, 'lig_properties/num_unpaired_hbd')
+        self.hba = self.getdata(bindingsite, 'lig_properties/num_hba')
+        self.unpaired_hba = self.getdata(bindingsite, 'lig_properties/num_unpaired_hba')
+        self.hal = self.getdata(bindingsite, 'lig_properties/num_hal')
+        self.unpaired_hal = self.getdata(bindingsite, 'lig_properties/num_unpaired_hal')
+        self.molweight = self.getdata(bindingsite, 'lig_properties/molweight')
+        self.logp = self.getdata(bindingsite, 'lig_properties/logp')
+        self.rotatable_bonds = self.getdata(bindingsite, 'lig_properties/num_rotatable_bonds')
+        self.rings = self.getdata(bindingsite, 'lig_properties/num_aromatic_rings')
+
+
+        # Binding Site residues
+        self.bs_res = []
+        bs_res = namedtuple('bs_res', 'resnr, reschain, aa, contact, min_dist')
+        for tagpart in bindingsite.xpath('bs_residues/bs_residue'):
+            resnumber, reschain = tagpart.text[:-1], tagpart.text[-1]
+            aa, contact, min_dist = tagpart.get('aa'), tagpart.get('contact'), tagpart.get('min_dist')
+            new_bs_res = bs_res(resnr=int(resnumber), reschain=reschain, aa=aa, contact=contact,
+                                min_dist=float(min_dist))
+            self.bs_res.append(new_bs_res)
+
+        # Interacting chains
+        self.interacting_chains = []
+        for chain in bindingsite.xpath('interacting_chains/interacting_chain'):
+            self.interacting_chains += chain.xpath('text()')
+
+        # Interactions
+        interactions = bindingsite.xpath('interactions')[0]
+        self.hydrophobics = [HydrophobicInteraction(x) for x in
+                             interactions.xpath('hydrophobic_interactions/hydrophobic_interaction')]
+        self.hbonds = [HydrogenBond(x) for x in interactions.xpath('hydrogen_bonds/hydrogen_bond')]
+        self.wbridges = [WaterBridge(x) for x in interactions.xpath('water_bridges/water_bridge')]
+        self.sbridges = [SaltBridge(x) for x in interactions.xpath('salt_bridges/salt_bridge')]
+        self.pi_stacks = [PiStacking(x) for x in interactions.xpath('pi_stacks/pi_stack')]
+        self.pi_cations = [PiCation(x) for x in interactions.xpath('pi_cation_interactions/pi_cation_interaction')]
+        self.halogens = [HalogenBond(x) for x in interactions.xpath('halogen_bonds/halogen_bond')]
+        self.metal_complexes = [MetalComplex(x) for x in interactions.xpath('metal_complexes/metal_complex')]
+        self.num_contacts = len(self.hydrophobics) + len(self.hbonds) + len(self.wbridges) + len(self.sbridges) + len(self.pi_stacks) + len(self.pi_cations) + len(self.halogens) + len(self.metal_complexes)
+        self.has_interactions = True if self.num_contacts > 0 else False
+
+        self.get_atom_mapping()
+
+    def get_atom_mapping(self):
+        """Parses the ligand atom mapping."""
+        # Atom mappings
+        smiles_to_pdb_mapping = self.bindingsite.xpath('mappings/smiles_to_pdb/text()')
+        if smiles_to_pdb_mapping == []:
+            self.mappings = {'smiles_to_pdb': None, 'pdb_to_smiles': None}
+        else:
+            smiles_to_pdb_mapping = {int(y[0]): int(y[1]) for y in [x.split(':') for x in smiles_to_pdb_mapping[0].split(',')]}
+            self.mappings = {'smiles_to_pdb': smiles_to_pdb_mapping}
+            self.mappings['pdb_to_smiles'] = {v: k for k, v in self.mappings['smiles_to_pdb'].items()}
+
+class PLIPXML(XMLStorage):
+    """Parses and stores all information from PLIP XML files."""
+    def __init__(self, xmlfile):
+        self.doc = etree.fromstring(self.load_xmlfile(xmlfile))
+
+        # Parse general information
+        self.version = self.getdata(self.doc, '/report/plipversion/')
+        self.pdbid = self.getdata(self.doc, '/report/pdbid')
+        self.filetype = self.getdata(self.doc, '/report/filetype')
+        self.fixed = self.getdata(self.doc, '/report/pdbfixes/')
+        self.filename = self.getdata(self.doc, '/report/filename')
+        self.excluded = self.getdata(self.doc, '/report/excluded_ligands')
+
+        # Parse binding site information
+        self.bsites = {BSite(bs, self.pdbid).bsid: BSite(bs, self.pdbid) for bs in self.doc.xpath('//bindingsite')}
+        self.num_bsites = len(self.bsites)
+
+
+    def load_xmlfile(self, xmlfile):
+        """Loads/parses an XML file and writes to a string if successful."""
+        try:
+            return etree.tostring(etree.parse(xmlfile))
+        except IOError:
+            sys.__stderr__.write("XML %s file could not be read." % xmlfile)
+            sys.exit(1)
---
+++ plip/test/test_xml_parser.py
@@ -0,0 +1,162 @@
+# coding=utf-8
+"""
+Protein-Ligand Interaction Profiler - Analyze and visualize protein-ligand interactions in PDB files.
+test_xml_parser.py - Unit Tests for XML Parser.
+Copyright 2014-2016 Sebastian Salentin
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+
+import unittest
+from plip.modules.plipxml import PLIPXML
+
+
+class XMLParserTest(unittest.TestCase):
+    """Checks if the XML parser is working correctly"""
+
+    def setUp(self):
+        self.px = PLIPXML('./xml/1vsn.report.xml')
+        self.bsite = self.px.bsites['NFT:A:283']
+        self.smiles = 'N=CCNC(=O)[C@H](CC(C)C)N[C@H](C(F)(F)F)c1ccc(cc1)c1ccc(cc1)S(=O)(=O)N'
+
+    def test_general_information(self):
+        """Test if general information is correctly parsed."""
+        self.assertEqual(self.px.version, '1.3.2')
+        self.assertEqual(self.px.pdbid, '1VSN')
+        self.assertEqual(self.px.filetype, 'PDB')
+        self.assertFalse(self.px.fixed)
+        self.assertEqual(self.px.filename, '1vsn.pdb')
+        self.assertEqual(self.px.excluded, None)
+
+    def test_bsite_information(self):
+        """Test if the binding site information is correctly parsed."""
+        self.assertEqual(self.bsite.pdbid, '1VSN')
+        self.assertEqual(self.bsite.uniqueid, '1VSN:NFT:A:283')
+        self.assertEqual(self.bsite.hetid, 'NFT')
+        self.assertEqual(self.bsite.longname, 'NFT')
+        self.assertEqual(self.bsite.ligtype, 'SMALLMOLECULE')
+        self.assertEqual(self.bsite.smiles, self.smiles)
+        self.assertEqual(self.bsite.members, ['NFT:A:283'])
+        self.assertFalse(self.bsite.composite)
+
+        # ligand properties
+        self.assertEqual(self.bsite.heavy_atoms, 33)
+        self.assertEqual(self.bsite.hbd, 5)
+        self.assertEqual(self.bsite.unpaired_hbd, 0)
+        self.assertEqual(self.bsite.hba, 7)
+        self.assertEqual(self.bsite.unpaired_hba, 2)
+        self.assertEqual(self.bsite.hal, 3)
+        self.assertEqual(self.bsite.unpaired_hal, 1)
+        self.assertEqual(self.bsite.rings, 2)
+        self.assertEqual(self.bsite.rotatable_bonds, 12)
+        self.assertEqual(self.bsite.molweight, 484.5349896)
+        self.assertEqual(self.bsite.logp, 6.0371)
+
+        # Atom mappings (non-exhaustive test)
+        lmap = self.bsite.mappings['pdb_to_smiles']
+        self.assertEqual(lmap[1625], 9)
+        self.assertEqual(lmap[1649], 1)
+        self.assertEqual(lmap[1617], 19)
+
+        # Binding site residues
+        self.assertEqual(len(self.bsite.bs_res), 36)
+
+        # Interacting chains
+        self.assertEqual(self.bsite.interacting_chains, ['A'])
+
+        # Has Interactions?
+        self.assertTrue(self.bsite.has_interactions, True)
+
+    def test_interactions(self):
+        """Test if interaction information is correctly parsed."""
+
+        # Hydrophobic Contacts
+        self.assertEqual(len(self.bsite.hydrophobics), 4)
+        hydrophobic1 = self.bsite.hydrophobics[0]
+        self.assertEqual(hydrophobic1.dist, 3.67)
+        self.assertEqual(hydrophobic1.resnr, 61)
+        self.assertEqual(hydrophobic1.restype, 'ASP')
+        self.assertEqual(hydrophobic1.reschain, 'A')
+        self.assertEqual(hydrophobic1.ligcarbonidx, 1639)
+        self.assertEqual(hydrophobic1.protcarbonidx, 448)
+        self.assertEqual(hydrophobic1.ligcoo, (-7.395, 24.225, 6.614))
+        self.assertEqual(hydrophobic1.protcoo, (-6.900, 21.561, 9.090))
+
+
+        # Hydrogen Bonds
+        self.assertEqual(len(self.bsite.hbonds), 7)
+        hbond1 = self.bsite.hbonds[0]
+        self.assertEqual(hbond1.resnr, 19)
+        self.assertEqual(hbond1.restype, 'GLN')
+        self.assertEqual(hbond1.reschain, 'A')
+        self.assertTrue(hbond1.sidechain)
+        self.assertEqual(hbond1.dist_h_a, 2.16)
+        self.assertEqual(hbond1.dist_d_a, 3.11)
+        self.assertEqual(hbond1.don_angle, 160.05)
+        self.assertTrue(hbond1.protisdon)
+        self.assertEqual(hbond1.donoridx, 153)
+        self.assertEqual(hbond1.donortype, 'Nam')
+        self.assertEqual(hbond1.acceptoridx, 1649)
+        self.assertEqual(hbond1.acceptortype, 'N2')
+        self.assertEqual(hbond1.ligcoo, (2.820, 18.145, 6.806))
+        self.assertEqual(hbond1.protcoo, (3.976, 15.409, 7.712))
+
+        # Water Bridges
+        self.assertEqual(len(self.bsite.wbridges), 2)
+        wbridge1 = self.bsite.wbridges[0]
+        self.assertEqual(wbridge1.resnr, 159)
+        self.assertEqual(wbridge1.restype, 'HIS')
+        self.assertEqual(wbridge1.reschain, 'A')
+        self.assertEqual(wbridge1.dist_a_w, 3.67)
+        self.assertEqual(wbridge1.dist_d_w, 3.13)
+        self.assertEqual(wbridge1.don_angle, 126.73)
+        self.assertEqual(wbridge1.water_angle, 116.36)
+        self.assertTrue(wbridge1.protisdon)
+        self.assertEqual(wbridge1.donor_idx, 1210)
+        self.assertEqual(wbridge1.donortype, 'Nar')
+        self.assertEqual(wbridge1.acceptor_idx, 1649)
+        self.assertEqual(wbridge1.acceptortype, 'N2')
+        self.assertEqual(wbridge1.ligcoo, (2.820, 18.145, 6.806))
+        self.assertEqual(wbridge1.protcoo, (6.401, 19.307, 4.971))
+        self.assertEqual(wbridge1.watercoo, (3.860, 18.563, 3.309))
+
+
+        # Salt Bridges
+        self.assertEqual(len(self.bsite.sbridges), 0)
+
+        # Pi stacking
+        self.assertEqual(len(self.bsite.pi_stacks), 0)
+
+        # Pi cation interactions
+        self.assertEqual(len(self.bsite.pi_cations), 0)
+
+        # Halogen Bonds
+        self.assertEqual(len(self.bsite.halogens), 2)
+        hal1 = self.bsite.halogens[0]
+        self.assertEqual(hal1.resnr, 67)
+        self.assertEqual(hal1.restype, 'TYR')
+        self.assertEqual(hal1.reschain, 'A')
+        self.assertTrue(hal1.sidechain)
+        self.assertEqual(hal1.dist, 3.37)
+        self.assertEqual(hal1.don_angle, 156.70)
+        self.assertEqual(hal1.acc_angle, 100.53)
+        self.assertEqual(hal1.don_idx, 1627)
+        self.assertEqual(hal1.donortype, 'F')
+        self.assertEqual(hal1.acc_idx, 485)
+        self.assertEqual(hal1.acceptortype, 'O3')
+        self.assertEqual(hal1.ligcoo, (-1.862, 29.303, 4.507))
+        self.assertEqual(hal1.protcoo, (-1.005, 26.276, 3.287))
+
+        # Metal complexes
+        self.assertEqual(len(self.bsite.metal_complexes), 0)
