6beada32812ea9d7b5f0f13df4462421044593df
Sebastian Salentin <sebastian.salentin@biotec.tu-dresden.de>
2017-11-30 14:01:58 +0100

    Changes license to GNU GPLv2

--- plip/modules/mp.py
+++ plip/modules/mp.py
@@ -1,19 +1,6 @@
 """
 Protein-Ligand Interaction Profiler - Analyze and visualize protein-ligand interactions in PDB files.
 mp.py - Functions for parallel processing
-Copyright 2014-2015 Sebastian Salentin, Joachim Haupt
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
 """
 
 # Python Standard Library
--- setup.py
+++ setup.py
@@ -1,38 +1,25 @@
 """
 Protein-Ligand Interaction Profiler - Analyze and visualize protein-ligand interactions in PDB files.
 setup.py - Setup configuration file for pip, etc.
-Copyright 2014 Sebastian Salentin
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
 """
 
 from setuptools import setup
 
 setup(name='plip',
-      version='1.3.4',
+      version='1.3.5',
       description='PLIP - Fully automated protein-ligand interaction profiler',
       classifiers=[
           'Development Status :: 5 - Production/Stable',
           'Intended Audience :: Science/Research',
           'Natural Language :: English',
-          'License :: OSI Approved :: Apache Software License',
+          'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
           'Programming Language :: Python :: 2.7',
           'Topic :: Scientific/Engineering :: Bio-Informatics'
       ],
       url='https://github.com/ssalentin/plip',
       author='Sebastian Salentin',
       author_email='sebastian.salentin@biotec.tu-dresden.de',
-      license='Apache',
+      license='GPLv2',
       packages=['plip', 'plip/modules'],
       scripts=['plip/plipcmd'],
       install_requires=[
