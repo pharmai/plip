48dff495c7d987f0b2c47635a0e30225dc73683b
Sebastian Salentin <sebastian.salentin@biotec.tu-dresden.de>
2017-12-13 17:02:05 +0100

    Correct version number

--- plip/modules/report.py
+++ plip/modules/report.py
@@ -14,7 +14,7 @@ import config
 # External libraries
 import lxml.etree as et
 
-__version__ = '1.3.4'
+__version__ = '1.3.5'
 
 class StructureReport:
     """Creates reports (xml or txt) for one structure/"""
