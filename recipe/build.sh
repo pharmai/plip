85ac9267fd984e04e665e1d81ac702cab62e90f7
lkagami <luciano.kagami@ufrgs.br>
2018-08-03 21:36:41 -0300

    Change setup file name

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,1 +1,1 @@
-$PYTHON $RECIPE_DIR/setup.py install --single-version-externally-managed --record=record.txt
+$PYTHON $RECIPE_DIR/setup_conda.py install --single-version-externally-managed --record=record.txt
