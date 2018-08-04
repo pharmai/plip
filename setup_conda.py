f4fbb9e21a6d86fb13d581fe3713510f9134d6cb
lkagami <luciano.kagami@ufrgs.br>
2018-08-03 21:37:23 -0300

    Setup file for anaconda

---
+++ setup_conda.py
@@ -0,0 +1,28 @@
+"""
+Protein-Ligand Interaction Profiler - Analyze and visualize protein-ligand interactions in PDB files.
+setup.py - Setup configuration file for pip, etc.
+"""
+
+from setuptools import setup, find_packages, Extension
+
+setup(name='plip',
+      version='1.4.2',
+      description='PLIP - Fully automated protein-ligand interaction profiler',
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Science/Research',
+          'Natural Language :: English',
+          'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.5',
+          'Topic :: Scientific/Engineering :: Bio-Informatics'
+      ],
+      url='https://github.com/ssalentin/plip',
+      author='Sebastian Salentin',
+      author_email='sebastian.salentin@tu-dresden.de',
+      license='GPLv2',
+      packages=['plip','plip.modules',],
+      keywords=['plip'],
+      include_package_data=True,
+      entry_points={'console_scripts': ['plip = plip.plipcmd_conda:main_init']},
+      zip_safe=False)
