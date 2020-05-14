da4459577e92c08193a11e0ae4276a02dfc7a657
fkaiser <contact@fkaiser.bio>
2020-05-14 16:59:04 +0200

    updates email address and branch names

--- plip/basic/config.py
+++ plip/basic/config.py
@@ -1,5 +1,5 @@
 __version__ = '2.0.0'
-__maintainer__ = 'PharmAI GmbH (2020) - www.pharm.ai - contact@pharm.ai'
+__maintainer__ = 'PharmAI GmbH (2020) - www.pharm.ai - hello@pharm.ai'
 
 import logging
 
