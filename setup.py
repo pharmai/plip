1f309f8ba953fca582c1d4aa91f0a8f46daf97c4
Alex Mestiashvili <alexandre.mestiashvili@tu-dresden.de>
2024-12-12 11:38:48 +0100

    Update setup.py, attempt to build ob bindings only if not loadable

--- setup.py
+++ setup.py
@@ -5,6 +5,50 @@ from distutils.command.build import build
 
 from plip.basic import config
 
+def install_pkg_via_pip(package):
+        import subprocess
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--user', package])
+
+def build_ob_py_bindings():
+    """ Fix openbabel issue (https://github.com/openbabel/openbabel/issues/2408) manually
+      - Download openbabel bindings from pypi
+      - extract to a tmpdir
+      - fix version in $tmpdir/openbabel-3.1.1.1/openbabel/__init__.py to have 2 dot version only
+      - install the fixed version with setuptools/pip
+      - cleanup the tmpdir
+    """
+    import requests
+    import tarfile
+    import tempfile
+    import shutil
+    import fileinput
+    import subprocess
+    import sys
+
+    openbabel_pypi_url='https://files.pythonhosted.org/packages/9d/3f/f08f5d1422d74ed0e1e612870b343bfcc26313bdf9efec9165c3ea4b3ae2/openbabel-3.1.1.1.tar.gz'
+
+    print (f"Downloading openbabel package from : {openbabel_pypi_url}")
+    obtar=requests.get(openbabel_pypi_url)
+    obtmpdir = tempfile.mkdtemp()
+    obtmp = obtmpdir+'/openbabel-3.1.1.1.tar'
+    open(obtmp,'wb').write(obtar.content)
+    print(f"Saving openbabel tar.gz to {obtmpdir}")
+    versfile = obtmpdir+'/openbabel-3.1.1.1/openbabel/__init__.py'
+
+    with tarfile.open(obtmp,mode='r') as tf:
+        tf.extractall(obtmpdir)
+
+    print ('Fix versions: s/3.1.1.1/3.1.1/ to make StrictVersion() in openbabel\'s setup.py happy')
+    print ('See https://github.com/openbabel/openbabel/issues/2408 for more details')
+    with fileinput.input(files=versfile,inplace=True) as f:
+        for line in f:
+            op = line.replace('__version__ = "3.1.1.1"', '__version__ = "3.1.1"')
+            print(op, end='')
+
+    install_pkg_via_pip(obtmpdir+'/openbabel-3.1.1.1/')
+    print (f"Cleanup tmpdir: {obtmpdir}")
+    shutil.rmtree(obtmpdir)
+
 class CustomBuild(build):
     """Ensure build_ext runs first in build command."""
     def run(self):
@@ -18,45 +62,15 @@ class CustomInstall(install):
         install.run(self)
 
 class CustomBuildExt(build_ext):
-    """ A workaround to build openbabel python3 bindings, for details see:
-    https://github.com/openbabel/openbabel/issues/2408
-    """
-
+    """ Check if openbabel bindings are installed || build them """
     def run(self):
-        import requests
-        import tarfile
-        import tempfile
-        import shutil
-        import fileinput
-        import subprocess
-        import sys
-
-        openbabel_pypi_url='https://files.pythonhosted.org/packages/9d/3f/f08f5d1422d74ed0e1e612870b343bfcc26313bdf9efec9165c3ea4b3ae2/openbabel-3.1.1.1.tar.gz'
-
-        def install(package):
-            subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--user', package])
-
-        print (f"Downloading openbabel package from : {openbabel_pypi_url}")
-        obtar=requests.get(openbabel_pypi_url)
-        obtmpdir = tempfile.mkdtemp()
-        obtmp = obtmpdir+'/openbabel-3.1.1.1.tar'
-        open(obtmp,'wb').write(obtar.content)
-        print(f"Saving openbable tar.gz to {obtmpdir}")
-        versfile = obtmpdir+'/openbabel-3.1.1.1/openbabel/__init__.py'
-
-        with tarfile.open(obtmp,mode='r') as tf:
-            tf.extractall(obtmpdir)
-
-        print ('Fix versions: s/3.1.1.1/3.1.1/ to make StrictVersion() happy')
-        print ('See https://github.com/openbabel/openbabel/issues/2408 for more details')
-        with fileinput.input(files=versfile,inplace=True) as f:
-            for line in f:
-                op = line.replace('__version__ = "3.1.1.1"', '__version__ = "3.1.1"')
-                print(op, end='')
-
-        install(obtmpdir+'/openbabel-3.1.1.1/')
-        print (f"Cleanup tmpdir: {obtmpdir}")
-        shutil.rmtree(obtmpdir)
+        try: import openbabel
+        except ModuleNotFoundError:
+            try: import requests
+            except ModuleNotFoundError:
+                install_pkg_via_pip('requests')
+            build_ob_py_bindings()
+        return
 
 setup(name='plip',
         version=config.__version__,
@@ -77,7 +91,6 @@ setup(name='plip',
         scripts=['plip/plipcmd.py'],
         cmdclass={'build': CustomBuild, 'build_ext': CustomBuildExt, 'install': CustomInstall},
         install_requires=[
-            'requests',
             'numpy',
             'lxml'
             ],
