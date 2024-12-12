0906cb5bf6d405198f3f86f998137db4edaa51be
Alex Mestiashvili <alexandre.mestiashvili@tu-dresden.de>
2024-12-12 11:58:41 +0100

    Fix indentation in setup.py

--- setup.py
+++ setup.py
@@ -73,30 +73,30 @@ class CustomBuildExt(build_ext):
         return
 
 setup(name='plip',
-        version=config.__version__,
-        description='PLIP - Fully automated protein-ligand interaction profiler',
-        classifiers=[
-            'Development Status :: 5 - Production/Stable',
-            'Intended Audience :: Science/Research',
-            'Natural Language :: English',
-            'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
-            'Programming Language :: Python :: 3.6',
-            'Topic :: Scientific/Engineering :: Bio-Informatics'
-            ],
-        url='https://github.com/pharmai/plip',
-        author='PharmAI GmbH',
-        author_email='hello@pharm.ai',
-        license='GPLv2',
-        packages=find_packages(),
-        scripts=['plip/plipcmd.py'],
-        cmdclass={'build': CustomBuild, 'build_ext': CustomBuildExt, 'install': CustomInstall},
-        install_requires=[
-            'numpy',
-            'lxml'
-            ],
-        entry_points={
-            "console_scripts": [
-                "plip = plip.plipcmd:main"
-                ]
-            },
-        zip_safe=False)
+    version=config.__version__,
+    description='PLIP - Fully automated protein-ligand interaction profiler',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Bio-Informatics'
+        ],
+    url='https://github.com/pharmai/plip',
+    author='PharmAI GmbH',
+    author_email='hello@pharm.ai',
+    license='GPLv2',
+    packages=find_packages(),
+    scripts=['plip/plipcmd.py'],
+    cmdclass={'build': CustomBuild, 'build_ext': CustomBuildExt, 'install': CustomInstall},
+    install_requires=[
+        'numpy',
+        'lxml'
+        ],
+    entry_points={
+        "console_scripts": [
+            "plip = plip.plipcmd:main"
+            ]
+        },
+    zip_safe=False)
